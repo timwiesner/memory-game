ef94313b9c54fe82d7f94399cd6585208a482641
Tim Wiesner <timothyjwiesner@gmail.com>
2017-08-16 15:26:50 -0500

    feat: add shuffle to squares
    Icons now shuffle randomly and areadded to squares in html file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,64 @@
-$('.square').click(function() {
-	alert('Event handler called!');
-})
+var squares = $('.square');
+
+var bus = $('<i class="fa fa-ambulance" aria-hidden="true"></i>');
+var plane = $('<i class="fa fa-plane" aria-hidden="true"></i>');
+var bitcoin = $('<i class="fa fa-btc" aria-hidden="true"></i>');
+var sissors = $('<i class="fa fa-scissors" aria-hidden="true"></i>');
+var piper = $('<i class="fa fa-pied-piper-alt" aria-hidden="true"></i>');
+var space = $('<i class="fa fa-space-shuttle" aria-hidden="true"></i>');
+var cutlery = $('<i class="fa fa-cutlery" aria-hidden="true"></i>');
+var camera = $('<i class="fa fa-camera-retro" aria-hidden="true"></i>');
+
+var bus2 = $('<i class="fa fa-ambulance" aria-hidden="true"></i>');
+var plane2 = $('<i class="fa fa-plane" aria-hidden="true"></i>');
+var bitcoin2 = $('<i class="fa fa-btc" aria-hidden="true"></i>');
+var sissors2 = $('<i class="fa fa-scissors" aria-hidden="true"></i>');
+var piper2 = $('<i class="fa fa-pied-piper-alt" aria-hidden="true"></i>');
+var space2 = $('<i class="fa fa-space-shuttle" aria-hidden="true"></i>');
+var cutlery2 = $('<i class="fa fa-cutlery" aria-hidden="true"></i>');
+var camera2 = $('<i class="fa fa-camera-retro" aria-hidden="true"></i>');
+
+
+var icons = [bus, plane, bitcoin, sissors, piper, space, cutlery, camera, bus2, plane2, bitcoin2, sissors2, piper2, space2, cutlery2, camera2];
+
+
+shuffle(icons);
+fillSquares(icons);
+
+
+function shuffle(arr) {
+  var j, shuffled;
+  for (var i = arr.length; i > 0; i--) {
+    j = Math.floor(Math.random() * i);
+    shuffled = arr[i - 1];
+    arr[i - 1] = arr[j];
+    arr[j] = shuffled;
+  }
+}
+
+function fillSquares(arr) {
+	squares.each(function(i) {
+		$(this).append(arr[i]);
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
