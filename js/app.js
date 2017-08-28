5808f9dcc20c0bda241f97cccfd021e7dd493ff6
Tim Wiesner <timothyjwiesner@gmail.com>
2017-08-28 16:03:01 -0500

    refactor: Change var to const and let

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 // Javascript Document
 
 // Define jQuery DOM variables
-var $squares = $('.square');
-var $restart = $('.fa-refresh');
-var $time = $('.timer');
-var $star = $('.fa-star');
+const $squares = $('.square');
+const $restart = $('.fa-refresh');
+const $time = $('.timer');
+const $star = $('.fa-star');
 
 // Define FontAwesome icons to be used for icons array
-var bus = $('<i class="fa fa-ambulance" aria-hidden="true"></i>');
-var plane = $('<i class="fa fa-plane" aria-hidden="true"></i>');
-var bitcoin = $('<i class="fa fa-btc" aria-hidden="true"></i>');
-var sissors = $('<i class="fa fa-scissors" aria-hidden="true"></i>');
-var piper = $('<i class="fa fa-pied-piper-alt" aria-hidden="true"></i>');
-var space = $('<i class="fa fa-space-shuttle" aria-hidden="true"></i>');
-var cutlery = $('<i class="fa fa-cutlery" aria-hidden="true"></i>');
-var camera = $('<i class="fa fa-camera-retro" aria-hidden="true"></i>');
+const bus = $('<i class="fa fa-ambulance" aria-hidden="true"></i>');
+const plane = $('<i class="fa fa-plane" aria-hidden="true"></i>');
+const bitcoin = $('<i class="fa fa-btc" aria-hidden="true"></i>');
+const sissors = $('<i class="fa fa-scissors" aria-hidden="true"></i>');
+const piper = $('<i class="fa fa-pied-piper-alt" aria-hidden="true"></i>');
+const space = $('<i class="fa fa-space-shuttle" aria-hidden="true"></i>');
+const cutlery = $('<i class="fa fa-cutlery" aria-hidden="true"></i>');
+const camera = $('<i class="fa fa-camera-retro" aria-hidden="true"></i>');
 
 // Define secondary FA icons to be matched
-var bus2 = $('<i class="fa fa-ambulance" aria-hidden="true"></i>');
-var plane2 = $('<i class="fa fa-plane" aria-hidden="true"></i>');
-var bitcoin2 = $('<i class="fa fa-btc" aria-hidden="true"></i>');
-var sissors2 = $('<i class="fa fa-scissors" aria-hidden="true"></i>');
-var piper2 = $('<i class="fa fa-pied-piper-alt" aria-hidden="true"></i>');
-var space2 = $('<i class="fa fa-space-shuttle" aria-hidden="true"></i>');
-var cutlery2 = $('<i class="fa fa-cutlery" aria-hidden="true"></i>');
-var camera2 = $('<i class="fa fa-camera-retro" aria-hidden="true"></i>');
+const bus2 = $('<i class="fa fa-ambulance" aria-hidden="true"></i>');
+const plane2 = $('<i class="fa fa-plane" aria-hidden="true"></i>');
+const bitcoin2 = $('<i class="fa fa-btc" aria-hidden="true"></i>');
+const sissors2 = $('<i class="fa fa-scissors" aria-hidden="true"></i>');
+const piper2 = $('<i class="fa fa-pied-piper-alt" aria-hidden="true"></i>');
+const space2 = $('<i class="fa fa-space-shuttle" aria-hidden="true"></i>');
+const cutlery2 = $('<i class="fa fa-cutlery" aria-hidden="true"></i>');
+const camera2 = $('<i class="fa fa-camera-retro" aria-hidden="true"></i>');
 
 // Define icons arrays
-var icons = [bus, plane, bitcoin, sissors, piper, space, cutlery, camera];
-var iconsTwo = [bus2, plane2, bitcoin2, sissors2, piper2, space2, cutlery2, camera2];
+let icons = [bus, plane, bitcoin, sissors, piper, space, cutlery, camera];
+let iconsTwo = [bus2, plane2, bitcoin2, sissors2, piper2, space2, cutlery2, camera2];
 
 // Concat into 'icons'
 icons = icons.concat(iconsTwo);
@@ -38,21 +38,21 @@ shuffle(icons);
 fillSquares(icons);
 
 // Set moves and score to zero
-var moves = 0;
-var score = 0;
+let moves = 0;
+let score = 0;
 
 // 'Boxes' to hold clicked icons
-var $clickedOne, $clickedTwo;
+let $clickedOne, $clickedTwo;
 
 // Shows icon upon click event
 // Inputs: user click on square
 // Outputs: calls clickCounter, answerOne and answerTwo to checkAnswer
-$squares.click(function(){
+$squares.click(function() {
   clickCounter();
   // ensure icon is not already shown
-  if (!$(this).children().hasClass('show')){
+  if (!$(this).children().hasClass('show')) {
     // check to see if clickedOne is undefined
-    if ($clickedOne === undefined){
+    if ($clickedOne === undefined) {
       // if yes, clickedOne = child i of square with class 'show'
       $clickedOne = $(this).children().addClass('show');
       // access clickedOne object, set answer = specific class
@@ -74,14 +74,14 @@ $squares.click(function(){
 // Inputs: user click on square
 // Outputs: time in seconds
 // Define start outside of function so clearInterval can be called
-var start;
-$squares.click(function(){
+let start;
+$squares.click(function() {
   // begins on first clicked square
-  if (moves === 1){
+  if (moves === 1) {
     // set timer = new Date
-    var timer = new Date;
+    let timer = new Date;
     // set interval counter
-    start = setInterval(function(){
+    start = setInterval(function() {
       // select timer html, update + 1 second
       $time.html(Math.floor((new Date - timer) / 1000) + " seconds");
     }, 1000);
@@ -91,17 +91,17 @@ $squares.click(function(){
 // Counts total number of moves
 // Inputs: none
 // Outputs: display stars
-function clickCounter(){
+function clickCounter() {
   // add one move
   moves++;
   // display for singular move
-  if (moves === 1){
+  if (moves === 1) {
     $('.moves').html(moves + " Move");
   } else {
     // subsequent moves
     $('.moves').html(moves + " Moves");
     // one star removed after 26 moves
-    if (moves > 26 && moves < 32){
+    if (moves > 26 && moves < 32) {
       $star.first().removeClass();
       // second star removed after 32 moves
     } else if (moves > 32){
@@ -114,7 +114,7 @@ function clickCounter(){
 // Keeps track of total score
 // Inputs: none
 // Outputs: alert message upon completion
-function trackScore(){
+function trackScore() {
   // updates time, move, and star count
   $('.fTime').html($('span.timer')[0].textContent);
   $('.fMoves').html(moves);
@@ -122,15 +122,15 @@ function trackScore(){
   // adds one point to score--one for each match
   score++;
   // initiates upon eight matches
-  if (score > 7){
+  if (score > 7) {
     // updates time only once final score reached
-    setTimeout(function(){
+    setTimeout(function() {
       // game-winning alert
       $('#myModal').css('display', 'block');
       $('span').click(function(){
         $('#myModal').css('display', 'none');
       });
-      $('.fa-play').click(function(){
+      $('.fa-play').click(function() {
         $('#myModal').css('display', 'none');
         $restart.trigger('click');
       });
@@ -143,7 +143,7 @@ function trackScore(){
 // Restart button handler and logic
 // Inputs: user click
 // Outputs: calls shuffle and fillSquares on icons
-$restart.click(function(){
+$restart.click(function() {
   // remove 'show' class from all squares
   $('.square i').removeClass('show');
   // undefine both clickedOne and clickedTwo
@@ -167,14 +167,14 @@ $restart.click(function(){
 // Checks answer one against answer two to look for match
 // Inputs: answerOne and answerTwo in the form of icon classes
 // Outputs: calls trackScore if correct
-function checkAnswers(answerOne, answerTwo){
-  if (answerOne === answerTwo){
+function checkAnswers(answerOne, answerTwo) {
+  if (answerOne === answerTwo) {
     // return clickedOne and clickedTwo to undefined state
     $clickedOne = undefined;
     $clickedTwo = undefined;
     trackScore();
   } else {
-    setTimeout(function(){
+    setTimeout(function() {
       // remove .show from clickedOne and clickedTwo
       $clickedOne.removeClass('show');
       $clickedTwo.removeClass('show');
@@ -188,9 +188,9 @@ function checkAnswers(answerOne, answerTwo){
 // Called if player moves on the same square twice
 // Inputs: none
 // Outputs: alert message
-function misClick(){
+function misClick() {
   alert('Please click a blank square.');
-  setTimeout(function(){
+  setTimeout(function() {
     // remove .show from clickedOne
     $clickedOne.removeClass('show');
     // return to undefined state after .show is removed
@@ -201,10 +201,10 @@ function misClick(){
 // Takes 'icons' array as inputs using the Fisher-Yates Shuffle
 // Inputs: icons array
 // Outputs: shuffled array
-function shuffle(arr){
+function shuffle(arr) {
   // Starter code obtained from https://stackoverflow.com/a/2450954/
-  var randomIndex, temporaryValue;
-  for (var i = arr.length; i > 0; i--){
+  let randomIndex, temporaryValue;
+  for (let i = arr.length; i > 0; i--) {
     randomIndex = Math.floor(Math.random() * i);
     temporaryValue = arr[i - 1];
     arr[i - 1] = arr[randomIndex];
@@ -216,9 +216,9 @@ function shuffle(arr){
 // Fills squares with shuffled icons
 // Inputs: shuffled array
 // Outputs: shuffled icons array to squares
-function fillSquares(arr){
+function fillSquares(arr) {
   // loops through each square
-  $squares.each(function(i){
+  $squares.each(function(i) {
     // places shuffled icons in squares
     $(this).append(arr[i]);
   });
